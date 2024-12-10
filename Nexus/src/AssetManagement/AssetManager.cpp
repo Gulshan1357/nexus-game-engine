285bcedb2125d322a94de4c4ac891b0e8aa66f70
Gulshan Sankhyan <g.sankhyan1357@gmail.com>
2024-12-10 14:22:50 -0500

    [Chore]: Add helper functions to AssetManger

--- Nexus/src/AssetManagement/AssetManager.cpp
+++ Nexus/src/AssetManagement/AssetManager.cpp
@@ -23,14 +23,29 @@ void AssetManager::ClearAssets()
 	m_sprites.clear();
 }
 
-void AssetManager::AddSprite(const std::string& assetId, const std::string& filePath, int columns, int rows)
+void AssetManager::AddSprite(const std::string& spriteId, const std::string& filePath, int columns, int rows)
 {
 	CSimpleSprite* sprite = App::CreateSprite(filePath.c_str(), columns, rows);
 
-	m_sprites.emplace(assetId, sprite);
+	m_sprites.emplace(spriteId, sprite);
 }
 
-CSimpleSprite* AssetManager::GetSprite(const std::string& assetId)
+CSimpleSprite* AssetManager::GetSprite(const std::string& spriteId)
 {
-	return m_sprites[assetId];
+	return m_sprites[spriteId];
+}
+
+float AssetManager::GetSpriteWidth(std::string spriteId)
+{
+	return GetSprite(spriteId)->GetWidth();
+}
+
+float AssetManager::GetSpriteHeight(std::string spriteId)
+{
+	return GetSprite(spriteId)->GetHeight();
+}
+
+void AssetManager::CreateAnimation(std::string spriteId, const unsigned int id, const float speed, const std::vector<int>& frames)
+{
+	GetSprite(spriteId)->CreateAnimation(id, speed, frames);
 }
--- Nexus/src/AssetManagement/AssetManager.h
+++ Nexus/src/AssetManagement/AssetManager.h
@@ -14,9 +14,14 @@ public:
 	~AssetManager();
 
 	void ClearAssets();
-	void AddSprite(const std::string& assetId, const std::string& filePath, int columns, int rows);
 
-	CSimpleSprite* GetSprite(const std::string& assetId);
+	void AddSprite(const std::string& spriteId, const std::string& filePath, int columns, int rows);
+	CSimpleSprite* GetSprite(const std::string& spriteId);
+
+	float GetSpriteWidth(std::string spriteId);
+	float GetSpriteHeight(std::string spriteId);
+
+	void CreateAnimation(std::string spriteId, const unsigned int id, const float speed, const std::vector<int>& frames);
 
 private:
 	std::map<std::string, CSimpleSprite*> m_sprites;
